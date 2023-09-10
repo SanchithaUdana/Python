0723c5aed2af8a7dd9f87c2c4656b59424076f5e
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-09-10 23:03:23 +0530

    Type Convetion

---
+++ typeConvertion.py
@@ -0,0 +1,14 @@
+a = 10
+b = "20"
+condition = "True"
+
+c = a + int(b)
+print(c)
+print(type(c))
+
+print(type(str(a)))
+
+print(bool(condition))
+print(type(bool(condition)))
+
+print("Enter Your name", end="?")
