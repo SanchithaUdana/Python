cdc9088cde08716ded2c294461cb98e5f000252c
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-09-10 22:21:50 +0530

    variables and operators

---
+++ variables.py
@@ -0,0 +1,35 @@
+name = "sanchitha"
+print(name)
+
+a = 10
+b = 2
+c = a + b
+print(c)
+print(a + b)
+print(a % b)
+print(a ** b)
+print(a / b)
+print(a // b)
+
+x = True
+y = False
+
+# logical operator
+print(x and y)
+print(x or y)
+print(not x)
+
+print(x ^ y)  # XOR gate
+
+# type() function
+print(type(x ^ y))
+
+a = a + 12
+a += 12  # assignment operator
+
+num1, num2, name = 12, 34, "Sanchitha"
+print(num1, num2)
+print(name)
+
+
+
