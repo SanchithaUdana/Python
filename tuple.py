1da1a3c2a8ce2ed4e69a35ef9882f790db31be07
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-09-13 21:55:47 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -14,4 +14,11 @@ print("Index \t: ", index)
 print("Name \t: ", name)
 print("Dep \t: ", dep)
 
+# use count how many times given element available
+print(x.count("sanchitha"))
+
+# give element index
+print(x.index("sanchitha"))
+
+
 
