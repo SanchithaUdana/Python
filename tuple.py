965005c0a6e480c32f6f3534bd6a1e23b833a6b4
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-09-13 21:33:12 +0530

    tuples

---
+++ tuple.py
@@ -0,0 +1,17 @@
+# tuples in python
+
+x = (1, "sanchitha", "ict")
+
+print(type(x))
+
+print(x[0])
+
+# tuple expanding
+
+index, name, dep = x
+
+print("Index \t: ", index)
+print("Name \t: ", name)
+print("Dep \t: ", dep)
+
+
