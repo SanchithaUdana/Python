f8c8e564efd78ea3a0d033638f9aade5379e9e1b
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-01-24 12:36:40 +0530

    conditional statement

--- conditions.py
+++ conditions.py
@@ -1,2 +1,38 @@
-#  Conditional Statements
+#  if Conditional Statements
+
+age = 12
+
+if age >= 18:
+    print("You are Selected")
+else:
+    print("You are Not Selected")
+
+# Nested if Condition
+
+mark = 54
+
+if 0 <= mark < 35:
+    print("W")
+elif 35 <= mark < 55:
+    print("S")
+elif 55 <= mark < 65:
+    print("C")
+elif 65 <= mark < 75:
+    print("B")
+elif 75 <= mark <= 100:
+    print("A")
+else:
+    print("Invalid Mark")
+
+
+#  ternary operator
+
+height = 156
+job = "Your Job is " + ("Security" if height > 150 else "labor")
+print(job)
+
+experience = 3
+post = "Senior" if experience > 2 else "Associate"
+print("Your Post is " + post)
+
 
