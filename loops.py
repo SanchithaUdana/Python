777d6a889818718d2fb909b8735067d6b2588836
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-01-27 13:23:00 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -57,5 +57,32 @@ for s in range(1, 10):
     print(s)
 
 
+# example for loops
+
+y = [23, 45, 56, 21, 4, 1, 87]
+
+large = 0
+small = y[0]
+
+for i in y:
+    if i > large:
+        large = i
+
+    if i < small:
+        small = i
+
+print("large number is ", large)
+print("small number is ", small)
+
+# python have in-built function for find max and min number in a list
+
+# y = [23, 45, 56, 21, 4, 1, 87]
+# according to the list y
+
+print(max(y))  # output is 87
+print(min(y))  # output is 1
+print(sum(y))  # output is 237
+
+
 
 
