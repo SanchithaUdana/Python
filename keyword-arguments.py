7daf4f080b8fa7ee3abe891ca0a147f4b48538d8
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-01-28 20:50:24 +0530

    Update keyword-arguments.py

--- keyword-arguments.py
+++ keyword-arguments.py
@@ -1,9 +1,24 @@
 # Keyword Arguments
 # named argument values converted to a dictionary
-def my_form(**form):  # we used ** to define keyword arguments
-    print(form)
-    print(type(form))
-    print("\"Hello\"", form['name'])
+def my_form(**formD):  # we used ** to define keyword arguments
+    print(formD)
+    print(type(formD))
+    print("\"Hello\"", formD['name'])
 
 
 my_form(name="sanchitha", subject="ICT")
+# in this function, named parameters are converted into dictionary
+
+
+def form(name, age):
+    print(name)
+    print(age)
+
+
+details = {
+    "name": "Sanchitha",
+    "age": 45
+}
+
+form(**details)
+# in this function, dictionary is converted into named parameters
