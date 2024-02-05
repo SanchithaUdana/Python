771c555d39f6d40e5159222e769c3767f8591d11
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-02-05 19:08:20 +0530

    file

--- File-Handle.py
+++ File-Handle.py
@@ -1,12 +1,17 @@
-file = open("data.txt")
+file = open("data.txt",  )
 
 # content = file.read()
 # print(content)
 
 for i, line in enumerate(file):
-    print(f"line {i}", line)
+    print(f"line {i} ->", line)
 
 file.close()
+
+file2 = open("data.txt", "w")
+
+for i in range(0, 20):
+    file2.write(str(i) + "\n")
 
 
 
