782f6a93a1445864f88568331034f0dcaae4127b
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-02-14 21:22:57 +0530

    Update oop-class.py

--- oop-concept/oop-class.py
+++ oop-concept/oop-class.py
@@ -1,8 +1,19 @@
 # class name is must be start using capital letter
 class Dog:
-    pass
+    # class body
+    name = ""
+
+    def bark(self):  # self argument is automatically passed by python
+        print("baw baw")
 
 
 # object
 
 dog1 = Dog()
+dog1.name = "scooby"
+dog1.bark()
+
+# call function using class name
+# must pass the object name
+Dog.bark(dog1)
+
